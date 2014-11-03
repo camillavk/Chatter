6c8914dc408a011bab499a35d74013f2580abd0f
Camilla van Klinken <camillavk@gmail.com>
2014-11-03 17:54:16 +0000

    User signs up when logged out

--- spec/features/listing_messages_spec.rb
+++ spec/features/listing_messages_spec.rb
@@ -19,7 +19,7 @@ feature "User browses the list of available messages" do
 		expect(page).to have_content("Example")
 	end
 
-	xscenario "with the author" do 
+	xscenario "with the author" do
 		visit('/')
 		sign_in('samsam', 'cat')
 		add_peep("Example", "Testing")
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -5,7 +5,7 @@ include SessionHelpers
 
 feature "User signs up" do
 
-	xscenario "when logged out" do
+	scenario "when logged out" do
 		expect{ sign_up }.to change(User, :count).by(1)
 		expect(page).to have_content("Welcome, Sam")
 		expect(User.first.email).to eq("sam@example.com")
@@ -59,7 +59,7 @@ feature "User signs out" do
 								:password_confirmation => "cat")
 	end
 
-	xscenario "while being signed in" do 
+	xscenario "while being signed in" do
 		sign_in('samsam', 'cat')
 		click_button "Sign out"
 		expect(page).to have_content("Goodbye!")
